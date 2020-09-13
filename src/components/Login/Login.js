e52a64a3cc0ddfb22505419065a72d8dfd2592fa
rafsan <rafsanamin18@gmail.com>
2020-09-13 19:24:48 +0600

    commit in 44.2

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,34 @@
-import React, { useContext } from 'react';
-
+import React, { useContext } from "react";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "./firebase.config";
 
 const Login = () => {
-    return (
-        <div>
-            <h1>This is Login</h1>
-        </div>
-    );
+  const handleGoogleSignIn = () => {
+    firebase.initializeApp(firebaseConfig);
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(function (result) {
+        var token = result.credential.accessToken;
+        var user = result.user;
+        console.log(user);
+      })
+      .catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+      });
+  };
+  return (
+    <div>
+      <h1>This is Login</h1>
+      <button onClick={handleGoogleSignIn}>Sign up using google</button>
+    </div>
+  );
 };
 
 export default Login;
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBZGBXyizioFWruFQN4qNHhOJFVe15xeHY",
+  authDomain: "burj-al-arab-dc701.firebaseapp.com",
+  databaseURL: "https://burj-al-arab-dc701.firebaseio.com",
+  projectId: "burj-al-arab-dc701",
+  storageBucket: "burj-al-arab-dc701.appspot.com",
+  messagingSenderId: "164102421223",
+  appId: "1:164102421223:web:f073d94b4b08f51c464042",
+};
+
+export default firebaseConfig;
