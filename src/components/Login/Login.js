01dafbd24eccda7612dfe5650cb8982342490cee
rafsan <rafsanamin18@gmail.com>
2020-10-03 06:16:34 +0600

    49.7

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,40 +6,53 @@ import { UserContext } from "../../App";
 import { useHistory, useLocation } from "react-router-dom";
 
 const Login = () => {
-  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
-  const history = useHistory();
-  const location = useLocation();
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const history = useHistory();
+    const location = useLocation();
 
-  const { from } = location.state || { from: { pathname: "/" } };
-  if (firebase.apps.length === 0) {
-    firebase.initializeApp(firebaseConfig);
-  }
+    const { from } = location.state || { from: { pathname: "/" } };
+    if (firebase.apps.length === 0) {
+        firebase.initializeApp(firebaseConfig);
+    }
 
-  const handleGoogleSignIn = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
+    const handleGoogleSignIn = () => {
+        const provider = new firebase.auth.GoogleAuthProvider();
 
-    firebase
-      .auth()
-      .signInWithPopup(provider)
-      .then(function (result) {
-        const { displayName, email } = result.user;
-        const signedInUser = { name: displayName, email };
-        setLoggedInUser(signedInUser);
-        history.replace(from);
-      })
-      .catch(function (error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        var email = error.email;
-        var credential = error.credential;
-      });
-  };
-  return (
-    <div>
-      <h1>This is Login</h1>
-      <button onClick={handleGoogleSignIn}>Sign up using google</button>
-    </div>
-  );
+        firebase
+            .auth()
+            .signInWithPopup(provider)
+            .then(function (result) {
+                const { displayName, email } = result.user;
+                const signedInUser = { name: displayName, email };
+                setLoggedInUser(signedInUser);
+                storeAuthToken();
+                history.replace(from);
+            })
+            .catch(function (error) {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                var email = error.email;
+                var credential = error.credential;
+            });
+    };
+
+    const storeAuthToken = () => {
+        firebase
+            .auth()
+            .currentUser.getIdToken(/* forceRefresh */ true)
+            .then(function (idToken) {
+                sessionStorage.setItem("token", idToken);
+            })
+            .catch(function (error) {
+                // Handle error
+            });
+    };
+    return (
+        <div>
+            <h1>This is Login</h1>
+            <button onClick={handleGoogleSignIn}>Sign up using google</button>
+        </div>
+    );
 };
 
 export default Login;
